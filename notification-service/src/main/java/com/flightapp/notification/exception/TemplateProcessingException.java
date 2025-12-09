42e2038c8882ded7f4ff635dfccd08b4f0be9d76
Riddhima Bhanja <164170235+riddhimaBhanja@users.noreply.github.com>
2025-12-09 22:53:10 +0530

    Delete notification-service directory

--- notification-service/src/main/java/com/flightapp/notification/exception/TemplateProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.flightapp.notification.exception;
-
-public class TemplateProcessingException extends RuntimeException {
-    public TemplateProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
